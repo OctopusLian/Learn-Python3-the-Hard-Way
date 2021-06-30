da994890ed99e604bf566524656afeefe53380fc
OctopusLian <zoctopus@qq.com>
2021-06-30 08:01:18 +0800

    morning add ex 6-10

---
+++ src/ex10/ex10.py
@@ -0,0 +1,14 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line"
+backslash_cat = "I'm \\a \\ cat."
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+
+'''
+	I'm tabbed in.
+I'm split
+on a line
+I'm \a \ cat.
+'''
---
+++ src/ex9/ex9.py
@@ -0,0 +1,17 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ",days)
+print("Here are the months: ",months)
+
+'''
+Here are the days:  Mon Tue Wed Thu Fri Sat Sun
+Here are the months:  Jan
+Feb
+Mar
+Apr
+May
+Jun
+Jul
+Aug
+'''
